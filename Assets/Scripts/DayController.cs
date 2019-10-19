6f2d6e76519b1750fc15f02a5f7a9cc7ccf239d9
Bulut <karabiyik15@itu.edu.tr>
2019-10-19 04:38:02 +0300

    Add Digital clock. Add light for current date.

--- Assets/Scripts/DayController.cs
+++ Assets/Scripts/DayController.cs
@@ -1,16 +1,21 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.LWRP;
 using UnityEngine.UI;
 
 public class DayController : MonoBehaviour
 {
+    [SerializeField] private Days myDay;
     [SerializeField] private GameObject[] remainderPlaces;
     [SerializeField] private Canvas remainderCanvas;
     [SerializeField] private Canvas rightClickDayCanvas;
+    [SerializeField] private Light2D ringLight;
     void Awake()
     {
-
+        CheckDay();
+        DigitalClock.OnDayChanged += CheckDay;
     }
     void Start()
     {
@@ -21,5 +26,19 @@ public class DayController : MonoBehaviour
     void Update()
     {
 
+    }
+
+    private void CheckDay()
+    {
+        DateTime dateTime = DateTime.Now;
+        var day = dateTime.DayOfWeek.ToString().ToLower();
+        var result = day.Equals(myDay.ToString().ToLower());
+        ringLight.enabled = result;
+
+    }
+
+    private void OnDestroy()
+    {
+        if (DigitalClock.OnDayChanged != null) DigitalClock.OnDayChanged -= CheckDay;
     }
 }
