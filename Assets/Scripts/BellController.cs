b4217b7908f0c44264680a5cd8b4781b6d027541
Bulut <karabiyik15@itu.edu.tr>
2019-10-19 05:09:29 +0300

    Fix order of objects.

---
+++ Assets/Scripts/BellController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BellController : MonoBehaviour
+{
+    [SerializeField] private GameObject rightClickCanvas;
+
+
+    void Start()
+    {
+        rightClickCanvas.transform.position = transform.position + Vector3.right * 0.25f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnMouseOver()
+    {
+        if (Input.GetMouseButtonDown(1)) rightClickCanvas.SetActive(true);
+        Debug.Log("enter");
+    }
+
+    private void OnMouseExit()
+    {
+        rightClickCanvas.SetActive(false);
+    }
+}
