2a1db43a6c0142bce8d4c0c6b9712a441047f1c7
Bulut <karabiyik15@itu.edu.tr>
2019-10-19 03:08:06 +0300

    Set up the clock, canvas and some objects.

---
+++ Assets/Scripts/DayController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DayController : MonoBehaviour
+{
+    [SerializeField] private GameObject[] remainderPlaces;
+    [SerializeField] private Canvas remainderCanvas;
+    [SerializeField] private Canvas rightClickDayCanvas;
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
