f6eaf3f43684051890636b71d8f116e890582741
Bulut <karabiyik15@itu.edu.tr>
2019-10-27 06:00:44 +0300

    Add aspect ratio controller. Add borderless functionality.

---
+++ Assets/AspectRatioController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AspectRatioController : MonoBehaviour
+{
+    private int _lastWidth = 0;
+    private int _lastHeight = 0;
+
+    void Update()
+    {
+        int width = Screen.width;
+        int height = Screen.height;
+
+        if (_lastWidth != width) // if the user is changing the width
+        {
+            // update the height
+            var heightAccordingToWidth = width / 16.0f * 9.0f;
+            Screen.SetResolution(width, Mathf.FloorToInt(heightAccordingToWidth), false, 0);
+        }
+        else if (_lastHeight != height) // if the user is changing the height
+        {
+            // update the width
+            var widthAccordingToHeight = height / 9.0f * 16.0f;
+            Screen.SetResolution(Mathf.FloorToInt(widthAccordingToHeight), height, false, 0);
+        }
+
+        _lastWidth = width;
+        _lastHeight = height;
+    }
+}
---
+++ Assets/Borderless/WindowScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class WindowScript : MonoBehaviour, IDragHandler
+{
+  
+    private Vector2 _deltaValue = Vector2.zero;
+    private bool _maximized;
+
+    public void OnCloseBtnClick()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        Application.Quit();
+    }
+
+    public void OnMinimizeBtnClick()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+        BorderlessWindow.MinimizeWindow();
+    }
+
+    public void OnMaximizeBtnClick()
+    {
+        EventSystem.current.SetSelectedGameObject(null);
+
+        if (_maximized)
+            BorderlessWindow.RestoreWindow();
+        else
+            BorderlessWindow.MaximizeWindow();
+
+        _maximized = !_maximized;
+    }
+
+    public void OnDrag(PointerEventData data)
+    {
+        if (BorderlessWindow.framed)
+            return;
+
+        _deltaValue += data.delta;
+        if (data.dragging)
+        {
+            BorderlessWindow.MoveWindowPos(_deltaValue, Screen.width, Screen.height);
+        }
+    }
+}
